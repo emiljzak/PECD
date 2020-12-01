6137de9d4bedf71d5095692a53aa92cf2c729e06
Emil Zak <emil.zak@desy.de>
2020-12-01 11:37:54 +0100

    created basis set module

---
+++ pecd/basis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; fill-column: 120 -*-
+#
+# Copyright (C) 2020 Emil Zak <emil.zak@cfel.de>
+#
+
+
+class angbas():
+    """comment"""
+
+    def __init__(self):
+        pass
+    
+    def plot_bas(self):
+        """plot the angular basis"""
+
+    def orth_test(self):
+        """test orthogonality relations for the basis set"""
