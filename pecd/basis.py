f0240e04e13fd7e57d4db170f3562224f459e76b
Emil Zak <emil.zak@desy.de>
2020-12-08 10:34:04 +0100

    implemented radial grid matrix

--- pecd/basis.py
+++ pecd/basis.py
@@ -12,10 +12,23 @@ from matplotlib import cm, colors
 from mpl_toolkits.mplot3d import Axes3D
 from sympy.physics.quantum.spin import Rotation
 
+from sympy import symbols
+from sympy.core import S, Dummy, pi
+from sympy.functions.combinatorial.factorials import factorial
+from sympy.functions.elementary.trigonometric import sin, cos
+from sympy.functions.elementary.miscellaneous import sqrt
+from sympy.functions.special.gamma_functions import gamma
+from sympy.polys.orthopolys import (legendre_poly, laguerre_poly,
+                                    hermite_poly, jacobi_poly)
+from sympy.polys.rootoftools import RootOf
+from sympy.core.compatibility import range
+
+
 class angbas():
     """comment"""
 
     def __init__(self):
+        """To do: add here attributes of the basis such as lmin,lmax"""
         pass
     
     def orth_test(self):
@@ -88,8 +101,268 @@ class angbas():
         plt.show()
 
 
+class radbas():
+    """Class of radial basis functions"""
+    def __init__(self,nlobatto,nbins,binwidth,rshift):
+        """
+        nlobatto: (int) number of Gauss-Lobatto basis functions per bin
+        nbins: (int) nubmer of bins
+        """
+        self.nlobatto = nlobatto
+        self.nbins = nbins
+        self.binwidth = binwidth
+        self.rshift = rshift #shift of the radial grid away from 0.0 to avoid singularity in the potential (beginning of the radial grid)
+        
+
+    def r_grid(self):
+        """radial grid of Gauss-Lobatto quadrature points"""        
+        #return radial coordinate r_in for given i and n
+
+        x=np.zeros(self.nlobatto)
+        w=np.zeros(self.nlobatto)
+        x,w=self.gauss_lobatto(self.nlobatto,14)
+        w=np.array(w)
+        x=np.array(x) # convert back to np arrays
+        xgrid=np.zeros((self.nbins,self.nlobatto),dtype=float) # appropriate for row-major language
+        
+        bingrid = np.zeros(self.nlobatto)
+        bingrid = x*0.5*self.binwidth+0.5*self.binwidth
+
+        xgrid[:,] = bingrid
+
+        Translvec = np.zeros(self.nlobatto)
+        for i in range(len(Translvec)):
+            Translvec[i] = float(i) * self.binwidth 
+
+        for ibin in range(self.nbins):    
+            xgrid[ibin,:] += Translvec[ibin]
+
+        print('\n'.join([' '.join(["  %12.4f"%item for item in row]) for row in xgrid]))
+        
+        """for i in range(0,nbins):
+                for n in range(0,self.nlobatto):
+                    xgrid[i,n]=x[n]*0.5*Rbin+0.5*Rbin+float(i)*Rbin+epsilon
+        return xgrid"""
+
+
+    def f(self,n,i,r,rgrid): 
+        "calculate f_in(r). Input r can be a scalar or a vector (for quadpy quadratures) " 
+        self.nlobatto=globals.nlobatto 
+        
+        #print("shape of r is", np.shape(r), "and type of r is", type(r))
+
+        if np.isscalar(r):
+            prod=1.0
+            if  r>= rgrid[i][0] and r <= rgrid[i][nlobatto-1]:
+                for mu in range(0,nlobatto):
+                    if mu !=n:
+                        prod*=(r-rgrid[i][mu])/(rgrid[i][n]-rgrid[i][mu])
+                return prod
+            else:
+                return 0.0
+
+        else:
+            prod=np.ones(np.size(r), dtype=float)
+            for j in range(0,np.size(r)):
+                if  r[j] >= rgrid[i][0] and r[j] <= rgrid[i][nlobatto-1]:
+                    for mu in range(0,nlobatto):
+                        if mu !=n:
+                            prod[j]*=(r[j]-rgrid[i][mu])/(rgrid[i][n]-rgrid[i][mu])
+                else:
+                    prod[j]=0.0
+            return prod
+
+
+    def fp(self,n,i,k,rgrid):
+        "calculate d/dr f_in(r) at r_ik (Gauss-Lobatto grid) " 
+        if n!=k:
+            fprime=(rgrid[n]-rgrid[k])
+            prod=1.0e00
+            for mu in range(0,globals.nlobatto):
+                if mu !=k and mu !=n:
+                    prod=prod*(rgrid[k]-rgrid[mu])/(rgrid[n]-rgrid[mu])
+            fprime=fprime*prod
+        elif n==k:
+            fprime=1.0
+            summ=0.0e00
+            for mu in range(0,globals.nlobatto):
+                    if mu !=n:
+                        summ=summ+(rgrid[n]-rgrid[mu])**(-1)
+            fprime=fprime*summ
+        return fprime
+
+        
+    def gauss_lobatto(self,n, n_digits):
+        """
+        Computes the Gauss-Lobatto quadrature [1]_ points and weights.
+
+        The Gauss-Lobatto quadrature approximates the integral:
+
+        .. math::
+            \int_{-1}^1 f(x)\,dx \approx \sum_{i=1}^n w_i f(x_i)
+
+        The nodes `x_i` of an order `n` quadrature rule are the roots of `P'_(n-1)`
+        and the weights `w_i` are given by:
+
+        .. math::
+            &w_i = \frac{2}{n(n-1) \left[P_{n-1}(x_i)\right]^2},\quad x\neq\pm 1\\
+            &w_i = \frac{2}{n(n-1)},\quad x=\pm 1
+
+        Parameters
+        ==========
+
+        n : the order of quadrature
+
+        n_digits : number of significant digits of the points and weights to return
+
+        Returns
+        =======
+
+        (x, w) : the ``x`` and ``w`` are lists of points and weights as Floats.
+                The points `x_i` and weights `w_i` are returned as ``(x, w)``
+                tuple of lists.
+
+        Examples
+        ========
+
+        >>> from sympy.integrals.quadrature import gauss_lobatto
+        >>> x, w = gauss_lobatto(3, 5)
+        >>> x
+        [-1, 0, 1]
+        >>> w
+        [0.33333, 1.3333, 0.33333]
+        >>> x, w = gauss_lobatto(4, 5)
+        >>> x
+        [-1, -0.44721, 0.44721, 1]
+        >>> w
+        [0.16667, 0.83333, 0.83333, 0.16667]
+
+        See Also
+        ========
+
+        gauss_legendre,gauss_laguerre, gauss_gen_laguerre, gauss_hermite, gauss_chebyshev_t, gauss_chebyshev_u, gauss_jacobi
+
+        References
+        ==========
+
+        .. [1] https://en.wikipedia.org/wiki/Gaussian_quadrature#Gauss.E2.80.93Lobatto_rules
+        .. [2] http://people.math.sfu.ca/~cbm/aands/page_888.htm
+        """
+        x = Dummy("x")
+        p = legendre_poly(n-1, x, polys=True)
+        pd = p.diff(x)
+        xi = []
+        w = []
+        for r in pd.real_roots():
+            if isinstance(r, RootOf):
+                r = r.eval_rational(S(1)/10**(n_digits+2))
+            xi.append(r.n(n_digits))
+            w.append((2/(n*(n-1) * p.subs(x, r)**2)).n(n_digits))
+
+        xi.insert(0, -1)
+        xi.append(1)
+        w.insert(0, (S(2)/(n*(n-1))).n(n_digits))
+        w.append((S(2)/(n*(n-1))).n(n_digits))
+        return xi, w
+        
+    def chi(self,n,i,r,rgrid,w):
+        # r is the argument f(r)
+        # rgrid is the radial grid rgrid[i][n]
+        # w are the unscaled lobatto weights
+        w=w*(0.5*globals.Rbin)#*sum(w[:])
+        val=np.zeros(np.size(r))
+        
+        if n==0 and i<globals.imax-1:
+            #print("bridge: ", n,i)
+            val=(f(globals.nlobatto-1,i,r,rgrid)+f(0,i+1,r,rgrid))*np.sqrt(float(w[globals.nlobatto-1])+float(w[0]))**(-1)/2.0
+            #print(type(val),np.shape(val))
+            return val 
+        elif n>0 and n<globals.nlobatto-1:
+            val=f(n,i,r,rgrid)*np.sqrt(float(w[n])**(-1))
+            #print(type(val),np.shape(val))
+            return val
+        else:
+            return val
+            
+
+
+        
+    def plot_chi(self):
+        plt.clf()
+        nlobatto=globals.nlobatto
+        epsilon=globals.epsilon
+        x=np.zeros(nlobatto)
+        w=np.zeros(nlobatto)
+        w=np.array(w)
+        x,w=gauss_lobatto(nlobatto,14)
+        rgrid=r_grid()
+        r=np.arange(epsilon, globals.Rmax+epsilon, 0.001)
+        y=np.zeros(np.size(r))
+        #print(rgrid)
+        #print(r)
+        #print(w)
+        #print(x)
+        #print("shape of w is", np.shape(w), "and type of w is", type(w))
+        w=np.array(w)
+        x=np.array(x)
+
+        #scale the weights
+        #w=w*globals.Rbin/2.0
+
+        plt.xlabel('r/a.u.')
+        plt.ylabel('Lobatto basis function')
+        plt.title('Lobatto functions')
+        plt.legend()   
+        print("imax = ",globals.imax)
+        
+        for i in range(0,globals.imax):
+            for n in range(0,globals.nlobatto):
+                for j in range(0,np.size(r)):
+                    y[j]=chi(n,i,r[j],rgrid,w)
+                plt.plot(r, y) 
+                plt.show()         
+
+
+    def plot_f(self):
+        nlobatto=globals.nlobatto
+        epsilon=globals.epsilon
+        x=np.zeros(nlobatto)
+        w=np.zeros(nlobatto)
+        w=np.array(w)
+        x,w=gauss_lobatto(nlobatto,14)
+        rgrid=r_grid()
+        print(rgrid)
+        r=np.arange(epsilon, globals.Rmax+epsilon, 0.001)
+        y=np.zeros(np.size(r))
+
+    
+        w=np.array(w)
+        x=np.array(x)
+
+
+
+        figLob = plt.figure()
+        plt.xlabel('r/a.u.')
+        plt.ylabel('Lagrange basis function')
+        plt.title('Lagrange basis functions')
+        plt.legend()   
+    
+
+        for n in range(0,globals.nlobatto):
+            for j in range(0,np.size(r)):
+                y[j]=f(n,0,r[j],rgrid)
+            plt.plot(r, y) 
+            plt.show()     
+
+    
+
+
 if __name__ == "__main__":      
     print("hello")
 
-    Ylm = angbas()    
-    Ylm.show_Y_lm(l=40,m=40)
+    #Ylm = angbas()    
+    #Ylm.show_Y_lm(l=40,m=40)
+
+    rbas = radbas(nlobatto = 10, nbins = 4, binwidth = 1.0, rshift=0.0)
+
+    rbas.r_grid()
