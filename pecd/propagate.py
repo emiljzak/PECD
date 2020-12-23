afa3c3ebcf5652a3bc2a91b754191b63f4431054
Emil Zak <emil.zak@desy.de>
2020-12-23 16:14:59 +0100

    added initial wavefunction

--- pecd/propagate.py
+++ pecd/propagate.py
@@ -7,9 +7,15 @@ import numpy as np
 import sys
 import matelem
 import timeit
+import quadpy
 from basis import radbas
 from matelem import mapping
+from scipy.special import genlaguerre,sph_harm
 import matplotlib.pyplot as plt
+from matplotlib import cm, colors
+from mpl_toolkits.mplot3d import Axes3D
+
+from numpy.polynomial.laguerre import laggauss
 
 class propagate(radbas,mapping):
 
@@ -119,20 +125,172 @@ class propagate(radbas,mapping):
         for edge, spine in ax.spines.items():
             spine.set_visible(False)
 
-        ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
-        ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+        #ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+        #ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
         ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
-        ax.tick_params(which="minor", bottom=False, left=False)
+        #ax.tick_params(which="minor", bottom=False, left=False)
 
         return im, cbar
 
 
-    def ini_wf(self,ini_state):
-        """ plot 2D array with color-coded magnitude"""
-        wf0=0
-        return wf0
+    def ini_wf(self,params,test=False):
+        psi0 = []
+        """ generate initial conditions for TDSE """
+        """psi0 (array: Nx4): l(int),m(int),i(int),n(int),s_xi(complex128) """
+        """ ordering n,i,l,m major"""
 
-   
+        if params['ini_state'] == "manual":
+            print("defining initial wavefunction manually \n")
+            psi0.append([0,0,0,0,1.0+0.0j])
+            print("initial wavefunction:")
+            print(psi0)
+
+        elif params['ini_state'] == "file":
+            print("reading initial wavefunction from file \n")
+            fl = open(params['ini_state_file_coeffs'],'r')
+
+            for line in fl:
+                words = line.split()
+                # columns: l, m, i, n, coef
+
+                l = int(words[0])
+                m = int(words[1])
+                i = int(words[2])
+                n = int(words[3])
+                coeff = float(words[4])
+                psi0.append([l,m,i,n,coeff])
+
+            #normalize the w-f
+
+            norm = 0.0
+            for ind in range(len(psi0)):
+                norm+=np.conj(psi0[ind][4]) * psi0[ind][4]
+
+            sum = 0 
+            for ind in range(len(psi0)):
+                psi0[ind][4] /= np.sqrt(norm)
+                sum += np.conj(psi0[ind][4]) * psi0[ind][4]
+            print(psi0)
+
+
+        elif params['ini_state'] == "projection":
+            print("generating initial wavefunction by projection of a given function onto our basis \n")
+
+            if test == True:
+                print("generating test wavefunction on a grid")
+      
+                """ plot the radial part """
+                h_radial = lambda r,n,l: (2*r/n)**l * np.exp(-r/n) * genlaguerre(n-l-1,2*l+1)(2*r/n)
+                radgrid = np.linspace(0.0,10.0,10)
+                thetagrid = np.linspace(0,np.pi,10)
+                phigrid  = np.linspace(0,2*np.pi,10)
+                grid3d = np.meshgrid(radgrid,thetagrid,phigrid)
+                nodes = list(zip(*(dim.flat for dim in grid3d)))
+                print(np.shape(nodes))
+                
+                nodes = np.asarray(nodes)
+
+                #for node in nodes:
+                #    print(node)
+                plt.plot(nodes[:,0],h_radial(nodes[:,0],3,0)**2 * nodes[:,0]**2)
+                #plt.show()
+
+                """plot the angular part"""
+                theta_2d, phi_2d = np.meshgrid(thetagrid, phigrid)
+                xyz_2d = np.array([np.sin(theta_2d) * np.sin(phi_2d), np.sin(theta_2d) * np.cos(phi_2d), np.cos(theta_2d)]) #2D grid of cartesian coordinates
+                colormap = cm.ScalarMappable( cmap=plt.get_cmap("cool") )
+                colormap.set_clim(-.45, .45)
+                limit = .5
+
+                plt.figure()
+                ax = plt.gca(projection = "3d")
+                
+                Y_lm = sph_harm(1,1, phi_2d,theta_2d)
+                print(np.shape(Y_lm))
+                #r = np.abs(Y_lm.real)*xyz_2d #calculate a point in 3D cartesian space for each value of spherical harmonic at (theta,phi)
+                #r = np.abs(iniwf(2.0,theta_2d, phi_2d))*xyz_2d #calculate a point in 3D cartesian space for each value of spherical harmonic at (theta,phi)  
+                r = self.custom_wf(1.0,theta_2d, phi_2d)*xyz_2d 
+                ax.plot_surface(r[0], r[1], r[2], facecolors=colormap.to_rgba(Y_lm.real), rstride=1, cstride=1)
+                ax.set_xlim(-limit,limit)
+                ax.set_ylim(-limit,limit)
+                ax.set_zlim(-limit,limit)
+                #plt.show()
+
+                #save function in wf0grid.txt
+
+                fl_out = open( params['ini_state_file_grid'],'w')
+                for i in range(len(nodes)):
+                    fl_out.write("%6.3f"%float(nodes[i][0])+"  %6.3f"%float(nodes[i][1])+"  %6.3f"%float(nodes[i][2])+" %6.3f"%self.custom_wf(nodes[i][0],nodes[i][1], nodes[i][2])+"\n")
+
+            # read coefficients from file
+
+            fl = open(params['ini_state_file_grid'],'r')
+            iniwf = []
+            for line in fl:
+                words = line.split()
+                # columns: l, m, i, n, coef
+
+                r = float(words[0])
+                theta = float(words[1])
+                phi = float(words[2])
+                wf = float(words[3])
+                iniwf.append([r,theta,phi,wf]) #initial wavefunction on a grid
+
+            """ project the wavefunction onto our basis """
+
+            """ 
+            input: complex wavefunction on r,theta,phi grid
+            return: set of coefficients in l,m,i,n basis""" 
+
+            mymap = mapping(int(params['lmin']), int(params['lmax']), int(params['nbins']), int(params['nlobatto']))
+            maparray, Nbas = mymap.gen_map()
+
+            for elem in maparray:
+                psi0.append([ elem[0], elem[1], self.overlap_grid(iniwf,elem[1],elem[0])])
+
+        return psi0
+
+    def overlap_grid(self,psi0,m,l):
+        Nlag = 10
+        x,w = laggauss(Nlag)
+        #psi0 is the projected function on the grid
+        myscheme = quadpy.u3.schemes["lebedev_025"]()
+
+        overlap = 0.0
+        for k in range(Nlag):
+            overlap += w[k] *  myscheme.integrate_spherical(lambda theta_phi: np.conjugate(sph_harm(m, l,  theta_phi[1]+np.pi, theta_phi[0])) * sph_harm(m, l,  theta_phi[1]+np.pi, theta_phi[0])  )
+
+        #print(myscheme)
+        """
+        Symbols: 
+                theta_phi[0] = theta in [0,pi]
+                theta_phi[1] = phi  in [-pi,pi]
+                sph_harm(m1, l1,  theta_phi[1]+np.pi, theta_phi[0])) means that we put the phi angle in range [0,2pi] and the  theta angle in range [0,pi] as required by the scipy special funciton sph_harm
+        """
+        return overlap
+
+    def custom_wf(self,r,theta,phi):
+        """ return custom function in the hydrogen atom orbital basis"""
+        h_radial = lambda r,n,l: (2*r/n)**l * np.exp(-r/n) * genlaguerre(n-l-1,2*l+1)(2*r/n)
+        nmax = 2
+        mmax = 1 
+        lmax = 1
+        f = 0.0
+        counter =0
+        """wavefunction definition"""
+        for n in range(1,nmax+1):
+            for l in range(0,n):
+                counter += 1
+                for m in range(-l,l+1):
+                    print(n,l,m)
+
+                    f +=   h_radial(r,n,l) * sph_harm(m,l, phi,theta)
+        f /= np.sqrt(float(counter)) 
+        return f.real
+
+
+
+
 
     def plot_wf_2d(self):
         pass
@@ -208,17 +366,19 @@ if __name__ == "__main__":
     """====basis set parameters===="""
 
 
-    params['nlobatto'] = 5
-    params['nbins'] = 5 #bug
+    params['nlobatto'] = 4
+    params['nbins'] = 1 #bug when nbins > nlobatto  
     params['binwidth'] = 4.0
     params['rshift'] = 1e-3 #rshift must be chosen such that it is non-zero and does not cover significant probability density region of any eigenfunction.
 
     params['lmin'] = 0
-    params['lmax'] = 0
+    params['lmax'] = 2
     
     """====runtime controls===="""
     params['method'] = "static"
-    params['ini_state'] = "manual" #or file
+    params['ini_state'] = "projection" #or "file" or "projection" or "manual"
+    params['ini_state_file_coeffs'] = "wf0coeffs.txt" # name of file with coefficients of the initial wavefunction in our basis
+    params['ini_state_file_grid'] = "wf0grid.txt" #initial wavefunction on a 3D grid of (r,theta,phi)
     params['basis'] = "prim" # or adiab
     params['potential'] = "hydrogen"
     params['scheme'] = "lebedev_025"
@@ -237,8 +397,9 @@ if __name__ == "__main__":
     params['int_rep_type'] = 'cartesian'
 
     hydrogen = propagate()
-    hmat = hydrogen.prop_wf(params)
-    hydrogen.plot_mat(np.abs(hmat[1:,1:]))
+    print(hydrogen.ini_wf(params,test=True))
+    #hmat = hydrogen.prop_wf(params)
+    #hydrogen.plot_mat(np.abs(hmat[1:,1:]))
     exit()
 
 
