cdc1f5196e3e42ac7a3e27910ab138a962490c4a
Emil Zak <emil.zak@desy.de>
2021-02-16 14:54:45 +0100

    implemented numpy and scipy eigensolver testing routines, sparse matrix generators and performance monitors

---
+++ pecd/eigensolvers.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8; fill-column: 120 -*-
+#
+# Copyright (C) 2021 Emil Zak <emil.zak@cfel.de>
+#
+import numpy as np
+import sys
+import time
+from scipy import linalg
+from scipy import sparse
+from scipy.sparse.linalg import eigsh
+""" In this module we are testing several eigensolvers for wavefunciton propagation """
+
+def gen_matrices():
+    """ this routine generates random sparse matrices for benchmarking """
+    N = 1000 #size of matrix
+    neval = 20 #number of requested eigenvalues
+    A = sparse.random(N,N, density=0.01)
+    AT = A
+    AT = np.transpose(AT)
+    A = 0.5 * (A + AT) 
+    print(type(A))
+    sparse.save_npz("mat_1k_dens1e-2.npz", A, compressed=False) 
+    eval, eigvec = eigsh(A, neval, M=None, sigma=None, which='LM')
+    Aloaded = sparse.load_npz("mat_1k_dens1e-2.npz")
+    evalloaded, eigvec = eigsh(Aloaded, neval, M=None, sigma=None, which='LM')
+    print(eval - evalloaded)
+    """ WARNING: you must use consistent saving and loading of npz files. Compression, if not handled consistently, can disturb the eigenvalues up to several %. """ 
+
+
+
+def test_direct(filename):
+    """this method compares the performance of numpy's eigh function with scipy's eigh and eigsh(sparse.) """
+    Asparse = sparse.load_npz(filename)
+    print(type(Asparse))
+
+    A = sparse.csc_matrix.toarray(Asparse)
+    print(type(A))
+    
+
+    t_start_process = time.process_time()  
+    t_start_time = time.time()
+    t_start_perf = time.perf_counter()
+    evalnp, eigvecnp = np.linalg.eigh(A, UPLO='L')
+    t_stop_process = time.process_time()  
+    t_stop_time = time.time()
+    t_stop_perf = time.perf_counter()
+
+    print(" ========= Numpy eigh =========")
+    print("time.time():" +  str("%10.3f"%(t_stop_time-t_start_time)) + "s")
+    print("time.process_time():" +  str("%10.3f"%(t_stop_process-t_start_process)) + "s")
+    print("time.perf_counter():" +  str("%10.3f"%(t_stop_perf-t_start_perf)) + "s")
+
+    t_start_process = time.process_time()  
+    t_start_time = time.time()
+    t_start_perf = time.perf_counter()
+    evalscipy, eigvecscipy = linalg.eigh(A)
+    t_stop_process = time.process_time()  
+    t_stop_time = time.time()
+    t_stop_perf = time.perf_counter()
+
+    print(" ========= Scipy eigh =========")
+    print("time.time():" +  str("%10.3f"%(t_stop_time-t_start_time)) + "s")
+    print("time.process_time():" +  str("%10.3f"%(t_stop_process-t_start_process)) + "s")
+    print("time.perf_counter():" +  str("%10.3f"%(t_stop_perf-t_start_perf)) + "s")
+    print(evalnp - evalscipy)
+
+filename = "mat_5k_dens1e-4.npz"
+
+#gen_matrices()
+
+test_direct(filename)
+
+exit()
+
+
+
+
+N = 5000 #size of matrix 
+neval = 20 #number of requested eigenvalues
+""" time.time() is most basis wall clock time with resolution dictated by linux clock. It returns absolute time.
+    time.perf_counter() is like time.time() but with higest possilbe resolution. They both contain sleep time , which means that also CPU load is reflected in it. This is relative time.
+    time.process_time() does not include sleep time, it includes both CPU and system time. This is relative time. It does not containt time used for instance to plot and see an image during run.
+    """
+
+t_start_process = time.process_time()  
+t_start_time = time.time()
+t_start_perf = time.perf_counter()
+
+
+# create a sparse matrix w0ith specific density
+A = sparse.random(N,N, density=0.0001)
+AT = A
+AT = np.transpose(AT)
+A = 0.5 * (A + AT)
+# visualize the sparse matrix with Spy
+plt.spy(A,color='r',markersize=0.1)
+plt.show()
+print(A)
+eval, eigvec = linalg.eigsh(A, neval, M=None, sigma=None, which='LM')
+print(eval)
+print("eigenvectors:")
+print(eigvec)
+t_stop_process = time.process_time()  
+t_stop_time = time.time()
+t_stop_perf = time.perf_counter()
+
+
+print("time.time():" +  str("%10.3f"%(t_stop_time-t_start_time)) + "s")
+print("time.process_time():" +  str("%10.3f"%(t_stop_process-t_start_process)) + "s")
+print("time.perf_counter():" +  str("%10.3f"%(t_stop_perf-t_start_perf)) + "s")
+
