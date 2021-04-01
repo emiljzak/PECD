abfc6f4d4abbdf4189a26ef3aada998d4936a539
Emil Zak <emil.zak@desy.de>
2021-04-01 21:01:46 +0200

    restructured input

---
+++ pecd/BOUND.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8; fill-column: 120 -*-
+#
+# Copyright (C) 2021 Emil Zak <emil.zak@cfel.de>
+#
+import numpy as np
+import sys
+import input
+
+
+
+def BUILD_HMAT0(params):
+    
+
+
+if __name__ == "__main__":      
+
+    params = input.gen_input()
+
+
