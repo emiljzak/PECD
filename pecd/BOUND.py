7ac5e10cf9451f9870dc581f447165d8fb685aba
Emil Zak <emil.zak@desy.de>
2021-04-02 09:41:22 +0200

    added POTENTIAL module, mapping functions and adaptive quads

--- pecd/BOUND.py
+++ pecd/BOUND.py
@@ -3,19 +3,150 @@
 #
 # Copyright (C) 2021 Emil Zak <emil.zak@cfel.de>
 #
-import numpy as np
-import sys
+
 import input
-import scipy.sparse
+import MAPPING
+import POTENTIAL
+
+import numpy as np
+from scipy import sparse
+
+import sys
+import time
 
 
 def BUILD_HMAT0(params):
 
-    hmat = csr_matrix((Nbas, Nbas), dtype=np.float64)
+    maparray, Nbas = MAPPING.GENMAP(params['bound_nlobs'],params['bound_nbins'],params['bound_lmax'],\
+        params['map_type'],params['working_dir'])
+
+    if params['hmat_format'] == 'csr':
+        hmat = sparse.csr_matrix((Nbas, Nbas), dtype=np.float64)
+    elif params['hmat_format'] == 'regular':
+        hmat = np.zeros((Nbas, Nbas), dtype=np.float64)
+
+
+    """ calculate hmat """
+    BUILD_POTMAT0(params)
+
+    """ diagonalize hmat """
+    start_time = time.time()
+    enr0, coeffs0 = np.linalg.eigh(hmat, UPLO = 'U')
+    end_time = time.time()
+    print("Time for diagonalization of field-free Hamiltonian: " +  str("%10.3f"%(end_time-start_time)) + "s")
+
+
+    if params['save_ham0'] == True:
+        if params['hmat_format'] == 'csr':
+            sparse.save_npz( params['working_dir'] + params['file_hmat0'] , hmat , compressed = False )
+        elif params['hmat_format'] == 'regular':
+            with open( params['working_dir'] + params['file_hmat0'] , 'w') as hmatfile:   
+                np.savetxt(hmatfile, hmat, fmt = '%10.4e')
+
+    if params['save_psi0'] == True:
+        psi0file = open(params['working_dir'] + params['file_psi0'], 'w')
+        for ielem,elem in enumerate(maparray):
+            psi0file.write( " %5d"%elem[0] +  " %5d"%elem[1] + "  %5d"%elem[2] + \
+                            " %5d"%elem[3] +  " %5d"%elem[4] + "\t" + \
+                            "\t ".join('{:10.5e}'.format(coeffs0[ielem,v]) for v in range(0,params['num_ini_vec'])) + "\n")
+
+    if params['save_enr0'] == True:
+        with open(params['working_dir'] + params['file_enr0'], "w") as energyfile:   
+            np.savetxt( energyfile, enr0 , fmt='%10.5f' )
+  
+    print(hmat)
+
+def BUILD_KEOMAT0(params):
+    print("under construction")
+
+
+def BUILD_POTMAT0(params):
+
+    print("Interpolating electrostatic potential")
+    esp_interpolant = POTENTIAL.INTERP_POT(params)
+
+    if  params['gen_adaptive_quads'] == True:
+        sph_quad_list = gen_adaptive_quads(params,esp_interpolant)
+
+
+
+
+
+
+def gen_adaptive_quads(params,esp_interpolant):
+
+    sph_quad_list = [] #list of quadrature levels needed to achieve convergence quad_tol of the potential energy matrix elements (global)
+
+    lmax = params['lmax']
+    quad_tol = params['sph_quad_tol']
+
+    print("Testing potential energy matrix elements using Lebedev quadratures")
+    #create list of basis set indices
+    anglist = []
+    for l in range(lmax-1,lmax+1):
+        for m in range(l-1,l+1):
+            anglist.append([l,m])
+
+    val =  np.zeros(shape=(len(anglist)**2),dtype=complex)
+    val_prev = np.zeros(shape=(len(anglist)**2),dtype=complex)
+
+    #pull out Lebedev schemes into a list
+    spherical_schemes = []
+    for elem in list(quadpy.u3.schemes.keys()):
+        if 'lebedev' in elem:
+            spherical_schemes.append(elem)
+    #print("Available schemes: " + str(spherical_schemes))
+
+    for i in range(np.size(self.rgrid,axis=0)): #iterate over radial indices in the basis: only l=0, m=0
+        for n in range(np.size(self.rgrid,axis=1)): 
+            rin = self.rgrid[i,n]
+            print(i,n,rin)
+            if rin <= self.params['r_cutoff']:
+                #iterate over the schemes
+                for scheme in spherical_schemes[3:]: #skip 003a,003b,003c rules
+
+                    k=0
+                    for l1,m1 in anglist:
+                        for l2,m2 in anglist:
+                        
+                            val[k] = self.calc_potmatelem_interp_vec(l1,m1,l2,m2,rin,scheme,esp_interpolant )
+                            print(" %4d %4d %4d %4d"%(l1,m1,l2,m2) + '%12.6f %12.6fi' % (val[k].real, val[k].imag)+ '%12.6f %12.6fi' % (val_prev[k].real, val_prev[k].imag) + \
+                                '%12.6f %12.6fi' % (np.abs(val[k].real-val_prev[k].real), np.abs(val[k].imag-val_prev[k].imag)) + '%12.6f '%np.abs(val[k]-val_prev[k])  )
+                            k+=1
+
+                    #check for convergence
+                    diff = np.abs(val - val_prev)
+
+                    if (np.any(diff>quad_tol)):
+                        print(str(scheme)+" convergence not reached") 
+                        for k in range(len(val_prev)):
+                            val_prev[k] = val[k]
+
+                    elif (np.all(diff<quad_tol)):     
+                        print(str(scheme)+" convergence reached!!!")
+                        val_conv = val_prev
+                        levels.append([i,n,str(scheme)])
+                        break
+
+                    #if no convergence reached raise warning
+                    if (scheme == spherical_schemes[len(spherical_schemes)-1] and np.any(diff>quad_tol)):
+                        print("WARNING: convergence at tolerance level = " + str(quad_tol) + " not reached for all considered quadrature schemes")
+            else:
+                scheme == spherical_schemes[0]
+                levels.append([i,n,str(scheme)])
+
+    print("Converged quadrature levels:")
+    print(levels)
+    quadfilename = self.params['working_dir']+self.params['quad_levels_file']
+    fname=open(quadfilename,'w')
+    print("saving quadrature levels to file: " + quadfilename )
+    for item in levels:
+        fname.write(str('%4d '%item[0]) +str('%4d '%item[1])+str(item[2]) +"\n")
+
 
 
 if __name__ == "__main__":      
 
     params = input.gen_input()
-
+    BUILD_HMAT0(params)
 
--- pecd/input.py
+++ pecd/input.py
@@ -15,6 +15,7 @@ def gen_input():
     """==== BOUND ===="""
 
     params['map_type'] = 'DVR' #DVR or SPECT
+    params['hmat_format']   = "regular" # regular, coo, csr
 
     """==== basis set parameters for BOUND ===="""
 
@@ -29,188 +30,42 @@ def gen_input():
     params['save_enr0']     = True #save eigenenergies for psi0
 
     params['num_ini_vec']   = 10 # number of initial wavefunctions (orbitals) stored in file
-    params['hmat_format']   = "csr" # regular, coo, csr
 
 
+    """==== potential energy matrix ===="""
+    params['gen_adaptive_quads'] = True
+    params['sph_quad_tol'] = 1e-3
 
 
     """==== electrostatic potential ===="""
     params['esp_file']      = "esp_grid_h2o_uhf_631Gss_10_0.5_com" #filename for the grid representation of the ESP
-    params['pot_type'] = "grid" #type of potential: analytic or grid
-    params['potential'] = "pot_hydrogen" # 1) pot_diagonal (for tests); 2) pot_hydrogen; 3) pot_null; 4) pot_grid_psi4_d2s
     params['r_cutoff'] = 8.0 #cut-off radius for the cation electrostatic potential. We are limited by the capabilities of psi4, memory. Common sense says to cut-off the ESP at some range to avoid spurious operations
     """ Note: r_cutoff can be infered from quad_levels file: when matrix elements of esp are nearly an overlap between spherical funcitons, it is good r_in for setting esp=0"""
 
+    params['plot_esp'] = False
+
+    """==== file paths and names ===="""
 
     params['file_psi0']     =   "psi0_" + params['molec_name']   + \
                                 "_"+str(params['bound_nbins'])   + \
                                 "_" + str(params['bound_nlobs']) + \
-                                "_" + str(params['binwidth'])    + \
+                                "_" + str(params['bound_binw'])    + \
                                 "_" + str(params['bound_lmax'])  + \
                                 "_" + params['esp_file']   + ".dat"
 
 
+    params['file_hmat0']    =   "hmat0_" + params['molec_name']   + \
+                                "_"+str(params['bound_nbins'])   + \
+                                "_" + str(params['bound_nlobs']) + \
+                                "_" + str(params['bound_binw'])    + \
+                                "_" + str(params['bound_lmax'])  + \
+                                "_" + params['esp_file']   + ".dat"
 
-    params['t0'] = 0.0 
-    params['tmax'] = 1.0 
-    params['dt'] = 0.1 
-    time_units = "as"
+    params['file_enr0']     =   "enr0_" + params['molec_name']   + \
+                                "_"+str(params['bound_nbins'])   + \
+                                "_" + str(params['bound_nlobs']) + \
+                                "_" + str(params['bound_binw'])    + \
+                                "_" + str(params['bound_lmax'])  + \
+                                "_" + params['esp_file']   + ".dat"
 
-
-
-
-
-    """==== spherical quadratures ===="""
-    params['scheme'] = "lebedev_011" #angular integration rule
-    params['adaptive_quad'] =  True#True: read degrees of adaptive angular quadrature for each radial grid point. Use them in calculations of matrix elements.
-    params['gen_adaptive_quads'] = False #generate and save in file a list of degrees for adaptive angular quadratures (Lebedev for now). This list is potential and basis dependent. To be read upon construction of the hamiltonian.
-    params['quad_tol'] = 1e-3 #tolerance threshold for the convergence of potential energy matrix elements (global) using spherical quadratures
-    params['atol'] = 1e-2 #absolute tolerance for the hamiltonian matrix to be non-symmetric
-    params['rtol'] = 1e-2 #relative tolerance for the hamiltonian matrix to be non-symmetric
-    params['quad_levels_file'] = "quad_levels_"+params['molec_name']+"_"+str(params['nbins'])+\
-                        "_"+str(params['nlobatto'])+"_"+str(params['lmax'])+"_"+\
-                            str(params['binwidth'])+"_"+ str('%5.2e'%params['quad_tol'])+\
-                                "_"+params['potential_grid']+".dat"
-
-
-
-
-    """===== TESTING ====="""
-    params['test_potmat_accur'] = False #Test the accuracy of potential energy matrix elements 
-    params['test_multipoles'] = False#test accuracy of potential energy matrix elements with multipole expansion of the potential and anlytic matrix elements
-    params['test_lebedev'] = False #test accuracy of potential energy matrix elements with lebedev quadrature and exact potential
-    params['multipoles_lmax'] = 6 #maximum L in the multipole expansion of the electrostatic potential
-    params['plot_esp'] = True #plot ESP?
-    params['calc_inst_energy'] = False #calculate instantaneous energy of a free-electron wavepacket?
-    params['test_pads'] = False #test convergence etc. for momentum-space distributions and PECD
-
-
-    """====molecule-field interaction hamiltonian===="""
-    params['int_rep_type'] = 'spherical' #representation of the molecule-field interaction potential (spherical or cartesian ): for now only used in calculations of instantaneous electron wavepacket energy.
-   
-
-    """===== post-processing ====="""
-
-    params['wavepacket_file'] = "wavepacket.dat" #filename into which the time-dependent wavepacket is saved
-    params['plot_modes'] = {"single_shot": False, "animation": False}
-
-    params['plot_types'] = { "radial": True,
-                             "angular": True,
-                             "r-radial_angular": True, 
-                             "k-radial_angular": True} #decide which of the available observables you wish to plot
-
-    params['plot_controls'] = { "plotrate": 1, 
-                                "plottimes": [100.0],#200.0,300.0,600.0,700.0,800.0,900.0,1000.0],
-                                "save_static": False,
-                                "save_anim": False,
-                                "show_static": True,
-                                "show_anim": False, 
-                                "static_filename": "obs",
-                                "animation_filename": "anim_obs"}
-
-    """ plotrate : rate of plotting observables in timestep units in animated plots
-        plottimes: times (in time_units) at which we plot selected observables in a static graph
-        save_static: save single shot plots to appropriate files (named separately for each plottime)
-        save_anim: save animation in a file
-        show_static: show single shot plots during the analysis
-        show_anim: show animation at the end of analysis
-        static_filename: name of the file into which the snapshots will be saved
-        animation_filename: name of the file into which animations will be saved
-    """
-
-    """=== Fourier transform ==="""
-    params['FT_method'] = "quadrature" #method for calculating the FT of the wavefunction: quadrature or fftn
-    params['FT_output'] = "lebedev_grid" #interpolate_cart, interpolate_sph, lebedev_grid: how do you want your output FT?
-    params['schemeFT_ang'] = "lebedev_025" #angular integration rule for calculating FT using the quadratures method
-    params['schemeFT_rad'] = ("Gauss-Hermite",10) #quadrature type for projection of psi(t) onto the lobatto basis: Gauss-Laguerre, Gauss-Hermite
-    params['schemeWLM'] = "lebedev_025" # 
-    params['test_quadpy_direct'] = False #compare quadpy integration with manual lebedev. Test convergence of angular part of the FT
-    params['test_conv_FT_ang'] = False #test spherical integration convergence in FT using quadratures
-
-    params['pecd_lmax'] = 2 #maximum angular momentum of the expansion into spherical harmonics of the momentum probability function
-    params['calculate_pecd'] = False #calculate FT of the wavefunction and expand it into spherical harmonics and calculate PECD?
-    params['time_pecd'] = params['tmax'] #at what time (in a.u.) do we want to calculate PECD?
-    params['WLM_quad_tol'] = 1e-4 #spherical quadrature convergence threshold for WLM(k_i)
-
-    params['kmax'] = 0.5 #maximum value of wavevector in FT (a.u.). This value is set as primary and other quantities should be estimated based on it.
-    #based on kmax and pulse duration we can estimate the spatial grid range:
-    Rmax_est = 6.0 *  params['tmax'] * time_to_au *  params['kmax'] 
-    print("An estimated range for spatial grid needed to cover " + str(params['kmax']) + " a.u. momentum wavepackets is: " + str("%10.0f"%(Rmax_est)) + " a.u.")
-   
-    params['nkpts'] = int( params['kmax'] *  params['nbins'] *  params['binwidth'] / (2.0 * np.pi) ) + 1
-    print("Number of points per dimension in grid representation of the 3D-FT of the wavepacket = " + str("%5d"%params['nkpts'])) 
-
-    #for prelimiary tests params['nkpts'] = 2
-    params['nkpts'] = 1
-    params['k-grid'] = np.linspace(0.5, params['kmax'],params['nkpts']) #range of momenta for the electron (effective radial range for the Fourier transform of the total wavefunction). Note that E = 1/2 * k^2, so it is easily convertible to photo-electron energy range
-    
-
-    """====field controls===="""
-    params['plot_elfield'] = False #plot z-component of the electric field
-    """ put most of what's below into a converion function """
-    params['omega'] =  23.128 #nm or eV
-
-    #convert nm to THz:
-    vellgt     =  2.99792458E+8 # m/s
-    params['omega']= 10**9 *  vellgt / params['omega'] # from wavelength (nm) to frequency  (Hz)
-    opt_cycle = 1.0e18/params['omega']
-    suggested_no_pts_per_cycle = 25     # time-step can be estimated based on the carrier frequency of the pulse. Guan et al. use 1000 time-steps per optical cycle (in small Krylov basis). We can use much less. Demekhin used 50pts/cycle
-    # 1050 nm = 1.179 eV = 285 THz -> 1 optical cycle = 3.5 fs
-    print("Electric field carrier frequency = "+str("%10.3f"%(params['omega']*1.0e-12))+" THz")
-    print("Electric field oscillation period (optical cycle) = "+str("%10.3f"%(1.0e15/params['omega']))+" fs")
-    print("suggested time-step for field linear frequency = "+str("%12.3f"%(params['omega']/1e12))+" THz is: " + str("%10.2f"%(opt_cycle/suggested_no_pts_per_cycle )) +" as")
-
-    params['omega'] *= 2.0 * np.pi # linear to angular frequency
-    params['omega'] /= 4.13e16 #Hz to a.u.
-    frequency_units = "nm" #we later convert all units to atomic unit
-
-    #params['E0'] = 1.0e9 #V/cm
-    field_units = "V/cm"
-
-    #convert from W/cm^2 to V/cm
-    epsilon0=8.85e-12
-    intensity = 7e14 #7e16 #W/cm^2 #peak intensity
-    field_strength = np.sqrt(intensity/(vellgt * epsilon0))
-    print("field strength")
-    print("  %8.2e"%field_strength)
-    params['E0'] = field_strength
-
-
-    #unit conversion
-    #params = const.convert_units(params)
-    time_to_au = time_to_au[time_units]
-
-    params['tmax'] *= time_to_au 
-    params['dt'] *= time_to_au
-    params['time_pecd'] *=time_to_au
-
-    #freq_to_au = freq_to_au[frequency_units]
-    #params['omega'] *= freq_to_au 
-    field_to_au = field_to_au[field_units]
-    params['E0'] *= field_to_au 
-    # 1a.u. (time) = 2.418 e-17s = 24.18 as
-    #field strength in a.u. (1a.u. = 5.1422e9 V/cm). For instance: 5e8 V/cm = 3.3e14 W/cm^2
-   
-    params['tau'] = 2000.0 #as: pulse duration
-
-    """==== field dictionaries ===="""
-    field_CPL = {"function_name": "fieldCPL", "omega": params['omega'], "E0": params['E0'], "CEP0": 0.0, "spherical": True, "typef": "LCPL"}
-    field_LP = {"function_name": "fieldLP", "omega": params['omega'], "E0": params['E0'], "CEP0": 0.0}
-
-    # if gaussian width is given: e^-t^2/sigma^2
-    # FWHM = 2.355 * sigma/sqrt(2)
-    env_gaussian = {"function_name": "envgaussian", "FWHM": 2.355 * params['tau']/np.sqrt(2.0) * time_to_au , "t0": 500.0 }
-
-    params['field_form'] = "analytic" #or numerical
-    params['field_type'] = field_CPL 
-    """ Available field types :
-        1) field_CPL
-        2) field_LP
-        3) field_omega2omega
-    """
-    params['field_env'] = env_gaussian 
-    """ Available envelopes :
-        1) env_gaussian
-        2) env_flat
-    """
     return params
