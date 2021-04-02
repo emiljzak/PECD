c888c45a11ed837e44fc0d25acea2668be669130
Emil Zak <emil.zak@desy.de>
2021-04-02 08:19:15 +0200

    implemented mapping functions

--- pecd/BOUND.py
+++ pecd/BOUND.py
@@ -6,11 +6,12 @@
 import numpy as np
 import sys
 import input
-
+import scipy.sparse
 
 
 def BUILD_HMAT0(params):
-    
+
+    hmat = csr_matrix((Nbas, Nbas), dtype=np.float64)
 
 
 if __name__ == "__main__":      
