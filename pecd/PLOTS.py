2137d1d4b0031deb4ae4e9480bd9a64a6ec23673
Emil Zak <emil.zak@desy.de>
2021-04-07 16:12:40 +0200

    plots with mayavi

--- pecd/PLOTS.py
+++ pecd/PLOTS.py
@@ -2,6 +2,7 @@
 from numpy import pi, sin, cos, mgrid
 from mayavi import mlab
 import numpy as np
+import os
 import input
 import GRID
 from scipy.special import sph_harm
@@ -131,15 +132,12 @@ def f(i,n,r,rgrid,nlobs,nbins):
         prod=np.ones(np.size(r), dtype=float)
         for j in range(0,np.size(r)):
 
-            if  r[j] >= rgrid[i,0] and r[j] <= rgrid[i,nlobs-1]:
+            for mu in range(0,nlobs):
+                if mu !=n:
+                    prod[j] *= (r[j]-rgrid[i,mu])/(rgrid[i,n]-rgrid[i,mu])
+                else:
+                    prod[j] *= 1.0
 
-                for mu in range(0,nlobs):
-                    if mu !=n:
-                        prod[j] *= (r[j]-rgrid[i,mu])/(rgrid[i,n]-rgrid[i,mu])
-                    else:
-                        prod[j] *= 1.0
-            else:
-                prod[j] = 0.0
     return prod
 
 
@@ -219,17 +217,175 @@ def plot_wf_ang(r0,coeffs,rgrid, nlobs,nbins):
 import numpy
 from mayavi.mlab import *
 
-def test_contour3d():
-    x, y, z = np.ogrid[-5:5:64j, -5:5:64j, -5:5:64j]
+def sph2cart(r,theta,phi):
+    x=r*np.sin(theta)*np.cos(phi)
+    y=r*np.sin(theta)*np.sin(phi)
+    z=r*np.cos(theta)
+    return x,y,z
 
-    scalars = x * x * 0.5 + y * y + z * z * 2.0
+def cart2sph(x,y,z):
+    r=np.sqrt(x**2+y**2+z**2)
+    theta=np.arctan(np.sqrt(x**2+y**2)/z)
+    phi=np.arctan(y/x)
+    return r,theta,phi
 
-    obj = contour3d(scalars, contours=10, transparent=True)
+def calc_wf_xyzgrid(nlobs,nbins,ivec,Gr,wffile,grid):
+    coeffs = read_coeffs(wffile,nvecs=4)
+
+    xl=np.zeros(nlobs)
+    w=np.zeros(nlobs)
+    xl,w=GRID.gauss_lobatto(nlobs,14)
+    w=np.array(w)
+
+    X, Y, Z = grid[0], grid[1], grid[2]
+    val = np.zeros((X.shape[0],1,1))
+    print(X.shape)
+
+    r,theta,phi = cart2sph(X,Y,Z)
+    #r= rx.flatten()
+    #theta =thetax.flatten()
+    #phi =phix.flatten()
+    print(theta.shape)
+    for ipoint in coeffs:
+
+        val +=  ipoint[5][ivec] * spharm(ipoint[3], ipoint[4], theta, phi).real / r
+    return val
+
+def test_contour3d(nlobs,nbins,Gr,wffile):
+    # The position of the atoms
+    atoms_x = np.array([1.0, 0.0, 0.0]) * 40 /5.5
+    atoms_y = np.array([0.0, 1.0, 0.0]) * 40 /5.5
+    atoms_z = np.array([0.0, 0.0, 0.0]) * 40 /5.5
+
+    #atoms_x = np.array([2.9, 2.9, 3.8]) * 40 /5.5
+    #atoms_y = np.array([3.0, 3.0, 3.0]) * 40 /5.5
+    #atoms_z = np.array([3.8, 2.9, 2.7]) * 40 /5.5
+
+    O = mlab.points3d(atoms_x[1:-1], atoms_y[1:-1], atoms_z[1:-1],
+                    scale_factor=3,
+                    resolution=20,
+                    color=(1, 0, 0),
+                    scale_mode='none')
+
+    H1 = mlab.points3d(atoms_x[:1], atoms_y[:1], atoms_z[:1],
+                    scale_factor=2,
+                    resolution=20,
+                    color=(1, 1, 1),
+                    scale_mode='none')
+
+    H2 = mlab.points3d(atoms_x[-1:], atoms_y[-1:], atoms_z[-1:],
+                    scale_factor=2,
+                    resolution=20,
+                    color=(1, 1, 1),
+                    scale_mode='none')
+    # The bounds between the atoms, we use the scalar information to give
+    # color
+    mlab.plot3d(atoms_x, atoms_y, atoms_z, [1, 2, 1],
+                tube_radius=0.4, colormap='Reds')
+
+
+    npts = 100j
+    xmax = 2.0
+    x, y, z = np.ogrid[-1.0*xmax:xmax:npts, -1.0*xmax:xmax:npts, -1.0*xmax:xmax:npts]
+    ivec = 2
+    #wf = calc_wf_xyzgrid(nlobs,nbins,ivec,Gr,wffile,[x,y,z])
+    wf = np.sin(x**2 + y**2 + 2. * z**2) / (z**2+x**2+y**2)
+    mlab.pipeline.volume(mlab.pipeline.scalar_field(wf), vmin=0.0, vmax=0.9)
+
     show()
-    return obj
+    #scalars =sin(x + y) + sin(2 * x - y) + cos(3 * x + 4 * y + z) 
+    #wf.reshape(10,10,10)
+    #obj = contour3d(wf, contours=5, transparent=False)
 
-test_contour3d()   
-exit()
+
+
+def test_contour_surf():
+    """Test contour_surf on regularly spaced co-ordinates like MayaVi."""
+    def f(x, y):
+        sin, cos = np.sin, np.cos
+        return sin(x + y) + sin(2 * x - y) + cos(3 * x + 4 * y)
+
+    x, y = np.mgrid[-7.:7.05:0.1, -5.:5.05:0.05]
+    s = contour_surf(x, y, f)
+    show()
+    return s
+
+
+
+def chemistry():
+    # Retrieve the electron localization data for H2O #############################
+    import os
+    if not os.path.exists('h2o-elf.cube'):
+        # Download the data
+        try:
+            from urllib import urlopen
+        except ImportError:
+            from urllib.request import urlopen
+        print('Downloading data, please wait')
+        opener = urlopen(
+            'http://code.enthought.com/projects/mayavi/data/h2o-elf.cube'
+            )
+        open('h2o-elf.cube', 'wb').write(opener.read())
+
+
+    # Plot the atoms and the bonds ################################################
+    import numpy as np
+    from mayavi import mlab
+    mlab.figure(1, bgcolor=(0, 0, 0), size=(350, 350))
+    mlab.clf()
+
+    # The position of the atoms
+    atoms_x = np.array([2.9, 2.9, 3.8]) * 40 / 5.5
+    atoms_y = np.array([3.0, 3.0, 3.0]) * 40 / 5.5
+    atoms_z = np.array([3.8, 2.9, 2.7]) * 40 / 5.5
+
+    O = mlab.points3d(atoms_x[1:-1], atoms_y[1:-1], atoms_z[1:-1],
+                    scale_factor=3,
+                    resolution=20,
+                    color=(1, 0, 0),
+                    scale_mode='none')
+
+    H1 = mlab.points3d(atoms_x[:1], atoms_y[:1], atoms_z[:1],
+                    scale_factor=2,
+                    resolution=20,
+                    color=(1, 1, 1),
+                    scale_mode='none')
+
+    H2 = mlab.points3d(atoms_x[-1:], atoms_y[-1:], atoms_z[-1:],
+                    scale_factor=2,
+                    resolution=20,
+                    color=(1, 1, 1),
+                    scale_mode='none')
+
+    # The bounds between the atoms, we use the scalar information to give
+    # color
+    mlab.plot3d(atoms_x, atoms_y, atoms_z, [1, 2, 1],
+                tube_radius=0.4, colormap='Reds')
+
+    # Display the electron localization function ##################################
+    fl = open('h2o-elf.cube','r')
+    print(fl.readlines()[9:])
+    # Load the data, we need to remove the first 8 lines and the '\n'
+    str = ' '.join(fl.readlines()[9:])
+    data = np.fromstring(str, sep=' ')
+    data.shape = (40, 40, 40)
+
+    source = mlab.pipeline.scalar_field(data)
+    min = data.min()
+    max = data.max()
+    vol = mlab.pipeline.volume(source, vmin=min + 0.65 * (max - min),
+                                    vmax=min + 0.9 * (max - min))
+
+    mlab.view(132, 54, 45, [21, 20, 21.5])
+
+    mlab.show()
+
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
+
+
 params = input.gen_input()
 wffile = params['working_dir'] + "psi0_h2o_1_20_10.0_4_uhf_631Gss.dat"
 
@@ -240,6 +396,10 @@ coeffs = read_coeffs(wffile,nvecs)
 Gr, Nr = GRID.r_grid( params['bound_nlobs'], params['bound_nbins'], params['bound_binw'],  params['bound_rshift'] )
 #plot_chi(0.0,params['bound_binw']*params['bound_nbins'],1000,Gr,params['bound_nlobs'], params['bound_nbins'])
 #exit()
+
+test_contour3d(params['bound_nlobs'], params['bound_nbins'],Gr,wffile)
+exit()
+
 r0 = 2.0
 plot_wf_ang(r0,coeffs,Gr,params['bound_nlobs'], params['bound_nbins'])
 exit()
