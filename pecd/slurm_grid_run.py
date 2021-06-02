55aed51c56db2dba7445e6c019b07ccc02e7085b
Emil Zak <zakemil@max-display003.desy.de>
2021-06-02 09:08:17 +0200

    cleanup

--- pecd/slurm_grid_run.py
+++ pecd/slurm_grid_run.py
@@ -7,14 +7,14 @@ def run_propagate(N_euler,N_batches):
 	path = os.getcwd()
 	print ("The current working directory is %s" % path)
 	START_FILES = os.listdir(path+"/slurm_run")
-	os.chdir(START_FILES)
+	os.chdir(path+"/slurm_run")
 	for ibatch in range(N_batches):
 
 		subprocess.call("./master_script.sh " + str(ibatch) + " " + str(N_batches) + " " + str(N_euler) , shell=True) 
 
 
 
-N_euler = 6
-N_batches = 2
+N_euler = 1
+N_batches = 1
 
 run_propagate(N_euler,N_batches)
