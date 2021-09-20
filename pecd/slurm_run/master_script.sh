e56e268b7d305fac1a4f88fee0f6ef699578de07
Emil Zak <emil.zak@cfel.de>
2021-09-20 13:35:14 +0200

    updated paths and arguments lists in master_script

--- pecd/slurm_run/master_script.sh
+++ pecd/slurm_run/master_script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#export exec="python3 /home/yachmena/RICHMOL/richmol/richmol.py"
-export exec="python3 /gpfs/cfel/cmi/scratch/user/zakemil/PECD/pecd/PROPAGATE.py"
+export exec="python3 /beegfs/desy/group/cfel/cmi/zakemil/PECD/pecd/PROPAGATE.py"
 export jobname="pecd_run"
 export pwd=`pwd`
 
-export jobtype="cfel-cmi,cfel,all"
+export jobtype="cfel-cmi,all"
 export nproc=32 #`nproc --all`
 export wclim=100
 export nnodes=1
@@ -17,7 +16,7 @@ echo "Requested number of nodes :" $nnodes
 echo "Requested number of cores :" $nproc
 echo "sbatch submit..."
 
-sbatch --partition=$jobtype --ntasks=$nproc --time=$wclim:00:00 --job-name=$jobname --output=$6$jobname.o --error=$6$jobname.e \
-       $pwd/run_python.sh $1 $2 $3 $4 $5 $6
+sbatch --partition=$jobtype --ntasks=$nproc --time=$wclim:00:00 --job-name=$jobname --output=$2$jobname.o --error=$2$jobname.e \
+       $pwd/run_python.sh $1 $2 
 
 #$pwd/run_python.sh
