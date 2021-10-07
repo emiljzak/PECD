fixed WDMATS in potential rotation part

pecd/BOUND.py
@@ -951,12 +951,13 @@ def BUILD_POTMAT0_ROT( params, maparray, Nbas , Gr, grid_euler, irun ):
     return potmat0, potind
 
 
-def gen_wigner_dmats(n_grid_euler, Jmax , grid_euler):
+def gen_wigner_dmats(n_grid_euler, Jmax, grid_euler):
 
     wigner = spherical.Wigner(Jmax)
+    grid_euler = grid_euler.reshape(-1,3)
     R = quaternionic.array.from_euler_angles(grid_euler)
     D = wigner.D(R)
-    #print(D.shape)
+
     WDMATS = []
     for J in range(Jmax+1):
         WDM = np.zeros((2*J+1,2*J+1,n_grid_euler), dtype=complex)
@@ -1053,17 +1054,19 @@ def test_vlm_symmetry(vLM):
 
 def rotate_tjmat(grid_euler,irun,tjmat):
     
-    Lmax = tjmat.shape[1]
-    lmax = tjmat.shape[0]
+    Lmax = tjmat.shape[1] -1
+
+    lmax = tjmat.shape[0] -1
+
     tjmat_rot = np.zeros( (lmax+1, Lmax+1, lmax+1, 2*Lmax + 1,  2 * lmax + 1), dtype = float)
     
     # pull wigner matrices at the given euler angle's set
     
     n_grid_euler = grid_euler.shape[0]
     print("number of euler grid points = " + str(n_grid_euler))
-    print("current Euler grid point = " + grid_euler[irun])
+    print("current Euler grid point = " + str(grid_euler[irun]))
 
-    WDMATS = gen_wigner_dmats(n_grid_euler, Lmax, grid_euler[irun])
+    WDMATS = gen_wigner_dmats(1, Lmax, grid_euler[irun])
 
 
     # transform tjmat
@@ -1073,7 +1076,7 @@ def rotate_tjmat(grid_euler,irun,tjmat):
                 for M in range(-L,L+1):
                     for m2 in range(-l2,l2+1):
                         for Mp in range(-L,L+1):
-                            tjmat_rot[l1,L,l2,M,m2] +=  WDMATS[L][Mp,M,irun] * tjmat[l1,L,l2,M,m2]
+                            tjmat_rot[l1,L,l2,M+L,m2+l2] +=  WDMATS[L][Mp+L,M+L,0] * tjmat[l1,L,l2,M+L,m2+l2]
 
     return tjmat_rot
 
@@ -1110,6 +1113,13 @@ def BUILD_POTMAT0_ANTON_ROT( params, maparray, Nbas , Gr, grid_euler, irun ):
     # 3. Build array of 3-j symbols
     tjmat       = gen_tjmat(params['bound_lmax'],params['multi_lmax'])
     tjmat_rot   = rotate_tjmat(grid_euler,irun,tjmat)
+
+
+    #print("tjmat-tjmatrot")
+    #print(np.allclose(tjmat,tjmat_rot))
+    #print("tjmatrot")
+    #print(tjmat_rot)
+    #exit()
 
     """ Testing of grid compatibility"""
     """