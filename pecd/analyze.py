fixed dtype bugs and ran tests of 2D vs 3D euler grids for Flm alpha averaged

pecd/analyze.py
@@ -100,8 +100,8 @@ class analysis:
         
                 #be careful when converting nested list to a numpy array.
                 G1.create_dataset(    name    = "Flm"+sigma, 
-                                            data    = np.asarray(temp_Flm_arr,dtype=float),
-                                            dtype   = float)
+                                            data    = np.asarray(temp_Flm_arr,dtype=complex),
+                                            dtype   = complex)
 
     def build_Flm_array(self,obs_list):
         """Return numpy array Flm[itime,l,m,k]"""

pecd/consolidate.py
@@ -104,7 +104,7 @@ class Avobs:
             G = h5.get('Flm_group')
             for sigma in self.helicity:
 
-                Flm_arr = np.asarray(G.get("Flm"+sigma),dtype=float)
+                Flm_arr = np.asarray(G.get("Flm"+sigma),dtype=complex)
                 Flm_dict[sigma] = Flm_arr
                 #print(list(G.items()))
 
@@ -115,6 +115,19 @@ class Avobs:
         "Calculating alpha-averaged Flm for a selected set of energies and times"
         Flm_alpha_av_dict = {'L':[],
                             'R':[]}
+
+        grid3D = self.grid_euler
+        Ngrid3D = grid3D.shape[0]
+        print("Ngrid3D = " + str(Ngrid3D))
+
+        print(grid3D)
+
+        #generate 2D euler grid for alpha-averaged variables
+        grid2D = self.grid_euler2D
+        Ngrid2D = grid2D.shape[0]   
+        print("Ngrid2D = " + str(Ngrid2D))
+
+        print(grid2D)
 
 
         return Flm_alpha_av_dict